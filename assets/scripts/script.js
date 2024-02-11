74ed2d396d4fb73eb5942d47636ce4de95a54304
DavudBabayev <davudbaba00@gmail.com>
2024-02-11 16:24:09 +0400

    6th Commit

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,12 +1,12 @@
 //////////MENU//////////
 let menu = document.querySelector(".menu")
-document.querySelector(".bi-list").addEventListener("click", ()=>{
+document.querySelector(".bi-list").addEventListener("click", () => {
     if (!menu.classList.contains("show")) {
         menu.classList.add("show")
     }
 })
 
-document.querySelector(".bi-x").addEventListener("click", ()=>{
+document.querySelector(".bi-x").addEventListener("click", () => {
     if (menu.classList.contains("show")) {
         menu.classList.remove("show")
     }
@@ -14,7 +14,7 @@ document.querySelector(".bi-x").addEventListener("click", ()=>{
 
 ////////Carousel-1/////////
 
-document.addEventListener("DOMContentLoaded", ()=> {
+document.addEventListener("DOMContentLoaded", () => {
     const items = document.querySelectorAll(".items");
     let currentIndex = 0;
     const totalItems = items.length;
@@ -38,7 +38,7 @@ document.addEventListener("DOMContentLoaded", ()=> {
 
 /////////VIDEO/////////
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const videoButton = document.querySelector("#video");
     const videoElement = document.querySelector("#fullscreen-video");
 
@@ -67,3 +67,62 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+
+//////////DATA//////////
+
+document.addEventListener('DOMContentLoaded', function () {
+    const pElements = document.querySelectorAll('p.pi');
+    getData('pi1');
+
+    pElements.forEach(function (pElement) {
+        pElement.addEventListener('click', function () {
+            if (!this.classList.contains('act')) {
+                pElements.forEach(function (p) {
+                    p.classList.remove('act');
+                });
+                this.classList.add('act');
+            }
+        });
+    });
+});
+
+let url1 = '  http://localhost:3000/main/';
+let url2 = '  http://localhost:3000/desserts/';
+let url3 = '  http://localhost:3000/drinks/';
+
+async function getData(id) {
+    let url = '';
+    if (id === 'pi1') {
+        url = url1;
+    } else if (id === 'pi2') {
+        url = url2;
+    } else if (id === 'pi3') {
+        url = url3;
+    }
+
+    try {
+        const res = await axios.get(url);
+        const data = res.data;
+
+        const cards = document.getElementById('cards');
+        cards.innerHTML = '';
+
+        data.forEach(e => {
+            cards.innerHTML +=`
+            <div class="card">
+            <div class="img">
+                <img src="${e.img}" alt="">
+            </div>
+            <div class="name">
+                <h2>${e.name}</h2>
+                <h3>${e.ing}</h3>
+            </div>
+            <p>$ ${e.price}</p>
+        </div>
+            `
+        });
+
+    } catch (error) {
+        console.error('Data is not found:', error);
+    }
+}
